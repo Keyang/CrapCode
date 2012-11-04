a3206256b4adea2ea68df3ad51bf4002c0be9bb5
U-Keyang-PC\Keyang <Keyang@Keyang-PC.(none)>
2012-11-04 00:42:13 +0000

    ui design; artworks; html page dev; client app dev <= on this

---
+++ src/web/js/app.js
@@ -0,0 +1,41 @@
+crapCode={};
+crapCode.app={
+    init:function(){
+       $(function(){
+            crapCode.template("listItem",function(){
+                crapCode.emit("ready");
+            });
+        }); 
+   }
+
+}
+crapCode.template=function(name,cb){
+    $.get("./template/"+name+".html",function(res,success,xhr){
+        var htmlCode=xhr.responseText;
+        $("body").append(htmlCode);
+        if (cb){
+            cb();
+        }
+    });
+}
+
+
+crapCode.on=function(event,cb){
+    if (!crapCode.events[event]){
+        crapCode.events[event]=[];
+    }
+    crapCode.events[event].push(cb);
+}
+
+crapCode.emit=function(event,args){
+    if (crapCode.events[event]){
+        for (var i=0;i<crapCode.events[event].length;i++){
+            var func=crapCode.events[event][i];
+            if (args === undefined){
+                args=[];
+            }
+            func.apply(crapCode,args);
+        }
+    }   
+}
+crapCode.app.init();
---
+++ src/web/js/list.js
@@ -0,0 +1,3 @@
+crapCode.on("ready",function(){
+    
+});
