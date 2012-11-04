c761e2002b91c790a74fd6bf0e4d1fe192c04060
Keyang Xiang <keyang.xiang@gmail.com>
2012-11-04 22:33:33 +0000

    auto loader issue solved

--- src/web/js/sh.js
+++ src/web/js/sh.js
@@ -1,38 +1,39 @@
-function path()
-{
-  var args = arguments,
-      result = []
-      ;
-       
-  for(var i = 0; i < args.length; i++)
-      result.push(args[i].replace('@', '/js/sh'));
-       
-  return result
-};
- 
-SyntaxHighlighter.autoloader.apply(null, path(
-  'applescript            @shBrushAppleScript.js',
-  'actionscript3 as3      @shBrushAS3.js',
-  'bash shell             @shBrushBash.js',
-  'coldfusion cf          @shBrushColdFusion.js',
-  'cpp c                  @shBrushCpp.js',
-  'c# c-sharp csharp      @shBrushCSharp.js',
-  'css                    @shBrushCss.js',
-  'delphi pascal          @shBrushDelphi.js',
-  'diff patch pas         @shBrushDiff.js',
-  'erl erlang             @shBrushErlang.js',
-  'groovy                 @shBrushGroovy.js',
-  'java                   @shBrushJava.js',
-  'jfx javafx             @shBrushJavaFX.js',
-  'js jscript javascript  @shBrushJScript.js',
-  'perl pl                @shBrushPerl.js',
-  'php                    @shBrushPhp.js',
-  'text plain             @shBrushPlain.js',
-  'py python              @shBrushPython.js',
-  'ruby rails ror rb      @shBrushRuby.js',
-  'sass scss              @shBrushSass.js',
-  'scala                  @shBrushScala.js',
-  'sql                    @shBrushSql.js',
-  'vb vbnet               @shBrushVb.js',
-  'xml xhtml xslt html    @shBrushXml.js'
-));
+function loadTemplate(tmpls){
+  var base="/js/sh/";
+  for (var i =0;i<tmpls.length;i++){
+    var url=base+tmpls[i];
+    $('body').append("<script src='"+url+"'></script>");
+  }
+}
+$(function(){
+  loadTemplate (
+   [
+      'shBrushAppleScript.js',
+      'shBrushAS3.js',
+      'shBrushBash.js',
+      'shBrushColdFusion.js',
+      'shBrushCpp.js',
+      'shBrushCSharp.js',
+      'shBrushCss.js',
+      'shBrushDelphi.js',
+      'shBrushDiff.js',
+      'shBrushErlang.js',
+      'shBrushGroovy.js',
+      'shBrushJava.js',
+      'shBrushJavaFX.js',
+      'shBrushJScript.js',
+      'shBrushPerl.js',
+      'shBrushPhp.js',
+      'shBrushPlain.js',
+      'shBrushPython.js',
+      'shBrushRuby.js',
+      'shBrushSass.js',
+      'shBrushScala.js',
+      'shBrushSql.js',
+      'shBrushVb.js',
+      'shBrushXml.js'
+
+]);
+});
+
+
