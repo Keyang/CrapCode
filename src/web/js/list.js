e2010398bb58e099607852e1dbbaf959f05a53d0
Keyang Xiang <keyang.xiang@gmail.com>
2012-11-04 23:07:59 +0000

    performance optimize

--- src/web/js/list.js
+++ src/web/js/list.js
@@ -1,19 +1,29 @@
 crapCode.on("ready",function(){
     $.get("/api/list",function(resArr){
+        var templateList=[];
+        var typeLoaded={};
         for (var i=0;i<resArr.length;i++){
             var res=resArr[i];
+            var type=res.codeType?res.codeType.toLowerCase():"text";
             var data={
                 dt:new Date(res.submitDateTime),
                 author:res.author?res.author : "匿名",
-                type:res.codeType?res.codeType.toUpperCase() : "TEXT",
+                type:type.toUpperCase(),
                 likenum:res.like,
                 code:res.codeRaw,
-                codetype:res.codeType
+                codetype:type
+            }
+            if (!typeLoaded[type]){
+                templateList.push(brushes[type]);
+                typeLoaded[type]=true;
             }
             var itemHtml=tmpl("listItemTemplate",data);
             $("#list").append(itemHtml);
         }
-        SyntaxHighlighter.highlight();    
+        loadTemplate(templateList,function(){
+            SyntaxHighlighter.highlight();        
+        });
+        
         
     });
 });
--- src/web/js/sh.js
+++ src/web/js/sh.js
@@ -1,39 +1,73 @@
-function loadTemplate(tmpls){
+function loadTemplate(tmpls,cb){
   var base="/js/sh/";
   for (var i =0;i<tmpls.length;i++){
+    var count=0;
+    count++;
     var url=base+tmpls[i];
-    $('body').append("<script src='"+url+"'></script>");
+    setTimeout(function(){
+      //$('body').append("<script src='"+url+"' onLoad='onTemplateLoaded'></script>");
+      $.getScript(url,function(){
+        count--;
+        if (count ===0){
+          if (cb){
+            cb();
+          }
+        }
+      });
+    },0)
+    
   }
 }
-$(function(){
-  loadTemplate (
-   [
-      'shBrushAppleScript.js',
-      'shBrushAS3.js',
-      'shBrushBash.js',
-      'shBrushColdFusion.js',
-      'shBrushCpp.js',
-      'shBrushCSharp.js',
-      'shBrushCss.js',
-      'shBrushDelphi.js',
-      'shBrushDiff.js',
-      'shBrushErlang.js',
-      'shBrushGroovy.js',
-      'shBrushJava.js',
-      'shBrushJavaFX.js',
-      'shBrushJScript.js',
-      'shBrushPerl.js',
-      'shBrushPhp.js',
-      'shBrushPlain.js',
-      'shBrushPython.js',
-      'shBrushRuby.js',
-      'shBrushSass.js',
-      'shBrushScala.js',
-      'shBrushSql.js',
-      'shBrushVb.js',
-      'shBrushXml.js'
 
-]);
-});
 
+  var brushes={
+    "applescript": "shBrushAppleScript.js",
+    "actionscript3": "shBrushAS3.js",
+    "as3": "shBrushAS3.js",
+    "bash": "shBrushBash.js",
+    "shell": "shBrushBash.js",
+    "coldfusion": "shBrushColdFusion.js",
+    "cf": "shBrushColdFusion.js",
+    "cpp": "shBrushCpp.js",
+    "c": "shBrushCpp.js",
+    "c#": "shBrushCSharp.js",
+    "c-sharp": "shBrushCSharp.js",
+    "csharp": "shBrushCSharp.js",
+    "css": "shBrushCss.js",
+    "delphi": "shBrushDelphi.js",
+    "pascal": "shBrushDelphi.js",
+    "diff": "shBrushDiff.js",
+    "patch": "shBrushDiff.js",
+    "pas": "shBrushDiff.js",
+    "erl": "shBrushErlang.js",
+    "erlang": "shBrushErlang.js",
+    "groovy": "shBrushGroovy.js",
+    "java": "shBrushJava.js",
+    "jfx": "shBrushJavaFX.js",
+    "javafx": "shBrushJavaFX.js",
+    "js": "shBrushJScript.js",
+    "jscript": "shBrushJScript.js",
+    "javascript": "shBrushJScript.js",
+    "perl": "shBrushPerl.js",
+    "pl": "shBrushPerl.js",
+    "php": "shBrushPhp.js",
+    "text": "shBrushPlain.js",
+    "plain": "shBrushPlain.js",
+    "py": "shBrushPython.js",
+    "python": "shBrushPython.js",
+    "ruby": "shBrushRuby.js",
+    "rails": "shBrushRuby.js",
+    "ror": "shBrushRuby.js",
+    "rb": "shBrushRuby.js",
+    "sass": "shBrushSass.js",
+    "scss": "shBrushSass.js",
+    "scala": "shBrushScala.js",
+    "sql": "shBrushSql.js",
+    "vb": "shBrushVb.js",
+    "vbnet": "shBrushVb.js",
+    "xml": "shBrushXml.js",
+    "xhtml": "shBrushXml.js",
+    "xslt": "shBrushXml.js",
+    "html": "shBrushXml.js"
+};
 
