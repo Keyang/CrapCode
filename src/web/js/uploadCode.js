fb251269b2185c1d370c6bde5b6af62c4cb7fa13
Keyang <keyang.xiang@gmail.com>
2012-11-07 20:44:17 +0000

    added missed assets;

---
+++ src/web/js/uploadCode.js
@@ -0,0 +1,57 @@
+crapCode.on("ready",function(){
+    var that=this;
+    $("#btn_upload_code").click(function(){
+        that.dlg("uploadCode",true,that.cleanUpload);
+    });
+    $("#upload_submit").click(function(){
+        var language=$("#codeLanguage").val();
+        var codeRaw=$("#crapcode").val();
+        that.uploadCode(codeRaw,language,function(isSuccess){
+            if (isSuccess){
+                that.dlg("uploadCode",false,that.cleanUpload);
+            }
+        });
+    });
+});
+crapCode.cleanUpload=function(){
+    setTimeout(function(){
+        $("#codeLanguage").val($("#codeLanguage option:first").val());
+        $("#crapcode").val("");
+    },1000);
+}
+crapCode.uploadCode=function(codeRaw,codeLanguage,cb){
+    if (codeRaw ==""){
+        alert("代码为空，请输入代码。");
+        if (cb){
+            cb(false);
+        }
+        return;
+
+    }
+    var data={"codeRaw":codeRaw,"codeLanguage":codeLanguage};
+    $.ajax({
+        type:"POST",
+        url:"/api/uploadcode",
+        data:data,
+        error:function(){
+            alert("代码上传失败。请稍候重试...");
+            if (cb){
+                cb(false);
+            }
+        },
+        success:function(res){
+            if (res.success){
+                alert("代码上传成功！您的代码会经过审核后发布。");
+                if (cb){
+                    cb(true);
+                }
+            }else{
+                alert("代码上传失败。错误信息："+res.errMsg);
+                if (cb){
+                    cb(false);
+                }
+            }
+            
+        }
+    });
+}
